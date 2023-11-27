048ece44c8b13130f49328ebc51b0e40171d9cf5
Suhas Shrinivasan <suhasshrinivasan@gmail.com>
2023-11-27 14:11:27 +0100

    Add basic sbatch script

---
+++ example_sbatch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=exmaple_ml_job
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1M
+#SBATCH --gpus=1
+#SBATCH --time=60
+
+module load singularity
+singularity exec --nv --env-file=<path_to_env_file> --bind <host-dir>:<contianer-dir> <singulairty-image.sif> <commannd_to_run>
